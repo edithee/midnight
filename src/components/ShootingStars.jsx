1b1a3e90f2a4a27dd995cb30dbd5c3ef73c6a907
midnightstarlight <malabaopie@gmail.com>
2024-03-29 12:17:00 +0800

    FET: add shooting stars bg component

---
+++ src/components/ShootingStars.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ShootingStars = () => {
+    return (
+        <section id="shootingstar">
+            <span id="shootingstarspan"></span>
+            <span id="shootingstarspan"></span>
+            <span id="shootingstarspan"></span>
+        </section >
+    )
+}
+
+export default ShootingStars
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import ParticlesComponent from "../components/Particle";
+import ShootingStars from "../components/ShootingStars";
 
 const MainLayout = () => {
     return (
         <>
             <Navbar />
             <Outlet />
+            <ShootingStars />
             <ParticlesComponent id="particles" />
         </>
     );
